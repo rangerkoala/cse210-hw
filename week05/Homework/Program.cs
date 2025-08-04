8639ae807e16f19242154faec81efb4ce5673599
rangerkoala <sebastiancamilopf@gmail.com>
2025-08-04 18:54:48 -0500

    w05 Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test the base Assignment class
+        Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+        Console.WriteLine();
+
+        // Test the MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test the WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
